c90bad7ab17a0b90ab9089517076f5662212f843
xuyongzhi <buaaxyz@yeah.net>
2019-08-22 10:18:59 +1000

    update config

--- sw_6c_bs1_lr1.sh
+++ sw_6c_bs1_lr1.sh
@@ -5,8 +5,7 @@ export PYTHONPATH=$PWD
 #TEST='--skip-test'
 #TEST='--only-test' 
 
-CONFIG_FILE='t1_sw6c/sw6c_fpn4321_bs1_lr10.yaml'
-#CONFIG_FILE='t1_sw6c/sw6c_fpn4321_p4_bs1_lr10.yaml'
+CONFIG_FILE='sw6c/sw6c_fpn4321_bs1_lr1.yaml'
 
 ipython tools/train_net_sparse3d.py -- --config-file "configs/$CONFIG_FILE"  $TEST
 
